71ca69c0fd106e4a1a8a234780d026b9cf937f8c
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-03-08 01:14:05 +0600

    chore(config, server.ts):connected database using .env, refector app.ts to app.ts and server.ts

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import path from "path";
+
+//process.cwd() to get current path and join .env to get full path
+dotenv.config({ path: path.join(process.cwd(), ".env") });
+
+export default {
+  port: process.env.PORT,
+  db_url: process.env.DATABASE_URL,
+};
