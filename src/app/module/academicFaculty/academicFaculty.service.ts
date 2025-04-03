a10bc8d83172bdebf5e96203d2e3eb26022e3990
mahbubur Rahman <inbx.mahbub@gmail.com>
2025-04-04 00:24:17 +0600

    feat(faculty): validate create faculty with already exists

--- src/app/module/academicFaculty/academicFaculty.service.ts
+++ src/app/module/academicFaculty/academicFaculty.service.ts
@@ -1,7 +1,16 @@
+import AppError from '../../error/AppError';
 import { TAcademicFaculty } from './academicFaculty.interface';
 import { AcademicFaculty } from './academicFaculty.model';
+import httpStatus from 'http-status';
 
 const createAcademicFacultyIntoDB = async (payload: TAcademicFaculty) => {
+  //isAlready Exists
+  const faculty = await AcademicFaculty.findOne({ name: payload.name });
+
+  if (faculty) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Faculty is already Exists');
+  }
+
   const result = await AcademicFaculty.create(payload);
   return result;
 };
